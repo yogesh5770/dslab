88cc531424c14a6c7125f692b1f4a28b209d6bf6
yogesh5770 <yogesh234456@gmail.com>
2026-04-27 08:00:57 +0530

    Update image.py

--- image.py
+++ image.py
@@ -1,13 +1,13 @@
 import tensorflow as tf
 
-# 1. Dataset setup
-data = r"C:\Users\yoges\Downloads\archive\Training"
+
+data = ""
 
 
 train = tf.keras.utils.image_dataset_from_directory(data, subset="training", validation_split=0.2, seed=1, image_size=(224,224))
 val = tf.keras.utils.image_dataset_from_directory(data, subset="validation", validation_split=0.2, seed=1, image_size=(224,224))
 
-# 2. Model structure
+
 model = tf.keras.Sequential([
     tf.keras.layers.Rescaling(1./255),
     tf.keras.layers.Conv2D(32, 3, activation='relu'), tf.keras.layers.MaxPooling2D(),
@@ -18,7 +18,7 @@ model = tf.keras.Sequential([
     tf.keras.layers.Dense(len(train.class_names), activation='softmax')
 ])
 
-# 3. Training
+
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 checkpoint = tf.keras.callbacks.ModelCheckpoint("best_model.h5", monitor='val_accuracy', save_best_only=True)
 
